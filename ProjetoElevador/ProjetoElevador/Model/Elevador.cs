dae5ef04cd6e0b5474e26bb2f0b44c7c1d5a77fc
Valsirene <valsirenesp@gmail.com>
2022-03-28 20:51:59 -0300

    Iniciando

---
+++ ProjetoElevador/ProjetoElevador/Model/Elevador.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador.Model
+{
+    class Elevador
+    {
+        public int andar_atual;
+        public int total_andares;
+        public int capacidade_elevador;
+        public int qtde_pessoas_elevador;
+
+        public void Inicializar(int capacidadeElevador, int totalAndares)
+        {
+            int andarAtual = andar_atual;
+            int qtdePessoasElev = qtde_pessoas_elevador;
+
+            Console.WriteLine("##### INICIALIZANDO #####");
+            Console.WriteLine("Capacidade do elevador: " + capacidadeElevador);
+            Console.WriteLine("Total de andares no prédio:" + totalAndares);
+            Console.WriteLine("Andar: TERREO: " + andarAtual);
+            Console.WriteLine("Pessoas no elevador: " + qtdePessoasElev);
+
+        }
+
+        public void Entrar(int capacidadeElevador, int qtdPessoasElevador, int qtdPessoasEntrar)
+        {
+            int espacoElevador = capacidadeElevador - qtdPessoasElevador;
+
+            if (espacoElevador >= qtdPessoasEntrar)
+            {
+                Console.WriteLine("Entraram {0}", qtdPessoasEntrar);
+            }
+            else
+            {
+                Console.WriteLine("Elevador lotado!!!");
+            }
+
+        }
+
+        public void Sair(int qtdPessoasElevador)
+        {
+            int passoasSairam;
+
+            if (qtdPessoasElevador > 0)
+            {
+                passoasSairam = (qtdPessoasElevador - (qtdPessoasElevador - 1));
+                Console.WriteLine("Saiu {0} pessoa do elevador.", passoasSairam);
+            }
+            else
+            {
+                Console.WriteLine("Ningúem no elevador!!!");
+            }
+
+        }
+
+        public void Subir(int andarAtual, int totalAndares)
+        {
+
+            if (andarAtual <= totalAndares)
+            {
+                andarAtual += 1;
+                Console.WriteLine("Andar atual: {0}", andarAtual);
+            }
+            else
+            {
+                Console.WriteLine("Último andar!!!");
+            }
+        }
+        public void Descer(int andarAtual)
+        {
+
+            if (andarAtual > 0)
+            {
+                andarAtual -= 1;
+                Console.WriteLine("Andar atual: {0}", andarAtual);
+            }
+            else
+            {
+                andarAtual = 0;
+                Console.WriteLine("Terreo!!!");
+            }
+        }
+    }
+}
---
+++ ProjetoElevador/ProjetoElevador/Program.cs
@@ -0,0 +1,41 @@
+﻿using ProjetoElevador.Model;
+using System;
+
+namespace ProjetoElevador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacidade_elevador = 5;
+            int total_andares = 10;
+            int andar_atual = 3;
+            int qtde_pessoas_elevador = 0;
+
+            Elevador elevador = new Elevador();
+            Console.WriteLine("############### PROJETO ELEVADOR ############");
+            elevador.andar_atual = andar_atual;
+            elevador.qtde_pessoas_elevador = qtde_pessoas_elevador;
+            elevador.Inicializar(capacidade_elevador, total_andares);
+
+            Console.WriteLine("############### ENTRANDO ############");
+            elevador.Entrar(capacidade_elevador, 2, 4);
+
+            Console.WriteLine("############### SAINDO ############");
+        
+            elevador.Sair(qtde_pessoas_elevador);
+
+            Console.WriteLine("############### SUBINDO ############");
+            
+            elevador.Subir(andar_atual, total_andares);
+
+            Console.WriteLine("############### DESCENDO ############");
+
+            elevador.Descer(andar_atual);
+
+
+
+
+        }
+    }
+}
